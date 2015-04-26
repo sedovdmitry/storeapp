ca0ef752c46d1f56e074f0018636703e049cd6d8
Dmitry Sedov <sedov.dmitry1@gmail.com>
2015-04-27 00:36:14 +0300

    Chapter 37. Domain Specific Language (DSL)

--- init.rb
+++ init.rb
@@ -1,6 +1,21 @@
 require_relative "store_application"
 
-StoreApplication.new
+StoreApplication.config do |app|
+
+  app.name        = "My Store"
+  app.environment = :development
+
+  app.admin do |admin|
+    admin.email = "admin@mystore.com"
+    admin.login = "admin"
+    admin.send_info_emails_on :mondays
+  end
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
 
 @items = []
 @items << AntiqueItem.new("car", price: 101, weight: 100)
@@ -15,4 +30,4 @@ cart.add_item RealItem.new({ :weight => 100, :price => 150, :name => "car" })
 cart.add_item RealItem.new({ :weight => 100, :price => 120, :name => "kettle" })
 
 method = "all_cars"
-p cart.send(method)
+#p cart.send(method)
--- store_application.rb
+++ store_application.rb
@@ -2,8 +2,9 @@ class StoreApplication
 
   class << self
 
-    def new 
+    def config
       unless @instance
+        yield(self)
         puts "loading files..."
         require_relative "string"
         require_relative "item_container"
@@ -17,6 +18,33 @@ class StoreApplication
       @instance ||= self
     end
 
+    attr_accessor :name, :environment
+
+    def admin(&block)
+      @admin ||= Admin.new(&block)
+    end
+
   end
+
+  class Admin
+
+      class << self
+
+        def new
+          unless @instance
+            yield(self)
+          end
+          @instance ||= self
+        end
+
+        attr_accessor :email, :login
+
+        def send_info_emails_on(day)
+          @send_info_emails_on = day
+        end
+
+      end
+    
+    end
 
 end
